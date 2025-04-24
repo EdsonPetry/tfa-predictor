8103e848fefc074f922e99f77cb4b46635f70f96
Edson Petry <elp95@amarel1.amarel.rutgers.edu>
2025-04-24 12:55:08 -0400

    Second HPO script run on Amarel

---
+++ hpo_results/mlp/jobs/trial_68/job_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=main
+#SBATCH --time=4:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=hpo_trial
+#SBATCH --output=%x-%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --env_setup=module purge && module load python/3.12.3 && cd /home/elp95/tfa-predictor
+#SBATCH --mail-user=edson.petry@rutgers.edu
+#SBATCH --mail-type=END,FAIL
+
+# Print some information about the job
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo SLURM job ID is $SLURM_JOB_ID
+
+# Load required modules
+module purge
+module load python/3.8.2
+
+# No virtual environment needed with system Python
+
+# Run the trial
+echo Starting trial...
+python -m hpo.run_trial --config /home/elp95/tfa-predictor/hpo_results/mlp/jobs/trial_68/config.json --output /home/elp95/tfa-predictor/hpo_results/mlp/jobs/trial_68/results.json
+
+echo Trial completed!
+exit 0
